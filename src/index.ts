9562f6655920ede515f974fa2d7f28095833820d
administrator <itPiligrim@gmail.com>
2022-02-26 09:05:58 +0300

    isArray update

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { FromAny, Convert, GetFrom, ChildObject, ChildArray, Validate } from "./from-any";
 export { FromAny, Convert, GetFrom, ChildObject, ChildArray, Validate };
 
-import { isString, isNumber, notEmpty, notEmptyArray } from "./validate";
-export { isString, isNumber, notEmpty, notEmptyArray };
+import { isString, isNumber, notEmpty, notEmptyArray, isArray } from "./validate";
+export { isString, isNumber, notEmpty, notEmptyArray, isArray };
 
 import { toFloat, toInt } from "./convert";
 export { toInt, toFloat };
